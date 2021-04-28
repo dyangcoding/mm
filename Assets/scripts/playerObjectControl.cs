e246f7b2b73f4fd5b0886c5b0dbf1341d729bd21
Dong Yang <Dong.Yang@Student.HTW-Berlin.de>
2021-04-28 13:14:12 +0200

    Add game and player object

---
+++ Assets/scripts/playerObjectControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerObjectControl : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Vector3 playerPos = new Vector3(0, 5f, 0);
+
+	// Use this for initialization
+	void Awake () {
+		rb = GetComponent<Rigidbody> ();
+	}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+	void Update () {
+		float xPos = transform.position.x + Input.GetAxis("Horizontal");
+        float yPos = transform.position.y + Input.GetAxis("Vertical");
+		playerPos = new Vector3 (xPos, yPos, 0);
+		transform.position = playerPos;
+	}
+}
---
+++ Assets/scripts/rotateCube2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateCube2 : MonoBehaviour
+{
+    public Transform myPrefab;
+    public float rotationRate = 100f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(myPrefab, new Vector3(0, 5f, 0), Quaternion.Euler(90, 0, 0));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.left, rotationRate * Time.deltaTime);
+    }
+}
