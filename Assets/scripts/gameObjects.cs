1a43d11564beca8d039b68496a1650de9b66b54e
Dong Yang <Dong.Yang@Student.HTW-Berlin.de>
2021-04-28 16:53:35 +0200

    generate random game objects with prefab

---
+++ Assets/scripts/gameObjects.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameObjects : MonoBehaviour
+{
+    public Transform myPrefab;
+    private float rotationRate = 50f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i=0; i < 5; i++) {
+            var position = new Vector3(Random.Range(-10.0f, 10.0f), 0, Random.Range(-10.0f, 10.0f));
+            Instantiate (myPrefab, position, Quaternion.identity);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myPrefab.RotateAround(myPrefab.position, Vector3.up, rotationRate * Time.deltaTime);
+    }
+}
--- Assets/scripts/playerObjectControl.cs
+++ Assets/scripts/playerObjectControl.cs
@@ -7,6 +7,8 @@ public class playerObjectControl : MonoBehaviour
     private Rigidbody rb;
     private Vector3 playerPos = new Vector3(0, 5f, 0);
 
+	private float rotationRate = 5f;
+
 	// Use this for initialization
 	void Awake () {
 		rb = GetComponent<Rigidbody> ();
@@ -20,9 +22,6 @@ public class playerObjectControl : MonoBehaviour
 
     // Update is called once per frame
 	void Update () {
-		float xPos = transform.position.x + Input.GetAxis("Horizontal");
-        float yPos = transform.position.y + Input.GetAxis("Vertical");
-		playerPos = new Vector3 (xPos, yPos, 0);
-		transform.position = playerPos;
+		transform.RotateAround(transform.position, Vector3.up, rotationRate * Time.deltaTime);
 	}
 }
