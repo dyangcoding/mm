326151c6eb9dd02e82f2d7085d198c51030ea40c
Dong Yang <Dong.Yang@Student.HTW-Berlin.de>
2021-05-18 20:59:59 +0200

    complete player object movement through virtual buttons

--- Assets/scripts/VirtualButtonController.cs
+++ Assets/scripts/VirtualButtonController.cs
@@ -1,18 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Vuforia;
 
 public class VirtualButtonController : MonoBehaviour
 {
+    public GameObject area;
+    public GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        VirtualButtonBehaviour[] vbs = GetComponentsInChildren<VirtualButtonBehaviour>();
+        for (int i = 0; i < vbs.Length; ++i)
+        {
+            vbs[i].RegisterOnButtonPressed(OnButtonPressed);
+            vbs[i].RegisterOnButtonReleased(OnButtonReleased);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
     {
-        
+        Debug.Log("OnButtonPressed: " + vb.VirtualButtonName);
+        switch (vb.VirtualButtonName)
+        {
+            case "moveFrontBtn":
+                player.transform.Translate(Vector3.forward * Time.deltaTime);
+                break;
+            case "moveBackBtn":
+                player.transform.Translate(Vector3.back * Time.deltaTime);
+                break;
+            case "rotateLeftBtn":
+                player.transform.Rotate(Vector3.up * Time.deltaTime);
+                break;
+            case "rotateRightBtn":
+                player.transform.Rotate(Vector3.down * Time.deltaTime);
+                break;
+        }
     }
+
+    /// <summary>
+    /// Called when the virtual button has just been released:
+    /// </summary>
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("OnButtonReleased: " + vb.VirtualButtonName);
+    }
+
 }
